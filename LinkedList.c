e26bd68f3b1c14950acd1fd064a76b0cfa06ccaa
Zach Newsom <zwnewsom@gmail.com>
2019-06-28 20:21:29 -0400

    initial commit

---
+++ LinkedList.c
@@ -0,0 +1,75 @@
+/* These 1st few lines are what's known as "pre-processor directives."
+ Suffice it to say that these 3 lines of code give you access to already written blocks of code
+ that we use throughout our program here. For example, the function printf() is contained in the stdio.h 
+ header file */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Here we're declaring our data object that we'll call "node." Each node will contain some data (in this case an integer
+ however we could use any data type or perhaps several data types if we so choose) and a pointer which will point to the 
+ next node in the list */
+typedef struct node
+{
+    struct node *next;
+    int data;
+} node;
+
+node *create_node(int num)
+{
+    node *new = calloc(1, sizeof(node));
+    new->data = num;
+    
+    return new;
+}
+
+node *tail_insert(node *head, int data)
+{
+    node *temp;
+    
+    if (head == NULL)
+        return create_node(data);
+    
+    temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    
+    temp->next = create_node(data);
+    
+    return head;//always return the head
+}
+
+void print_list(node *head)
+{
+}
+
+void print_list_recursive(node *head)
+{
+}
+
+int main(void)
+{
+    int i, r;
+
+    // The head of our linked list. If we don't initialize it to NULL, our
+    // tail_insert() function might segfault.
+    node *head = NULL;
+
+    srand(time(NULL));
+
+    // Populate the linked list with random integers. We are inserting into the
+    // head of the list each time.
+    for (i = 0; i < 10; i++)
+    {
+        printf("Inserting %d...\n", r = rand() % 20 + 1);
+        head = tail_insert(head, r);
+    }
+
+    // Print the linked list.
+    //print_list(head);
+
+    // Print the linked list using our recursive function.
+    //print_list_recursive(head);
+
+    return 0;
+}
